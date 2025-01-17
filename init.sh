4c16d7d65e85cff6ee70b6f8fd7ec91c206c3b06
dalgardnerd <thatiphoneguy@gmail.com>
2025-01-17 12:47:14 -0700

    move symlink location

--- init.sh
+++ init.sh
@@ -4,21 +4,27 @@ MAGENTA='\033[1;35m'
 RED='\033[1;31m'
 NOCOLOR='\033[0m'
 
+
+
 if [ ! -f "/config/quartz/quartz.config.ts" ]; then
+    mv /tmp/README.md /content/index.md
     printf "${MAGENTA}Initializing Quartz...${NOCOLOR}\n" 
     git clone https://github.com/jackyzha0/quartz.git
     cd quartz
     npm i
-    npx quartz create -X symlink -l shortest -s /config/content
+    npx quartz create -X symlink -l shortest -s /content
     printf "${CYAN}Quartz initialization complete.\n${NOCOLOR}\n"
     printf "${MAGENTA}Peforming initial quartz build...${NOCOLOR}\n"
     npx quartz build
     printf "${MAGENTA}Quartz initial build complete!${NOCOLOR}\n"
     
+    
 else
     printf "${MAGENTA}Quartz is already initialized. Checking for updates...${NOCOLOR}\n"
     cd quartz
     npx quartz update
+    printf "${MAGENTA}Updating quartz build...${NOCOLOR}\n"
+    npx quartz build
 fi
 
 printf "${CYAN}You can view the webpage at the following address: ${NOCOLOR} http://localhost\n"
