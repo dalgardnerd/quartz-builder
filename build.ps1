705fca00b9cbd7c1f5bfd206b88856fb65a44a98
dalgardnerd <thatiphoneguy@gmail.com>
2025-01-22 15:31:48 -0700

    stuff

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 docker buildx build ./quartz-builder --% -t dalgardnerd:quartz-builder:latest
-docker buildx build ./quartz-caddy --% -t dalgardnerd:quartz-caddy:latest
+docker buildx build ./quartz-caddy --% -t dalgardnerd:quartz-caddy:latestdocker buildx build ./quartz-builder --% -t dalgardnerd/quartz-builder:latest
+docker buildx build ./quartz-caddy --% -t dalgardnerd/quartz-caddy:latest
---
+++ quartz-caddy/init copy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+CYAN='\033[1;36m'
+MAGENTA='\033[1;35m'
+NOCOLOR='\033[0m'
+
+
+if [ ! -d "/opt/quartz/node_modules" ]; then
+        mkdir content
+        cd /tmp
+        mv index.md /opt/quartz/content
+        git clone https://github.com/jackyzha0/quartz.git
+        cp -r quartz/ /opt
+        rm  -rf *
+        cd /opt/quartz
+        npm i
+        npx quartz create -X new -l shortest
+        printf "${MAGENTA}Quartz initialization complete.\n${NOCOLOR}\n"
+        printf "${MAGENTA}Peforming initial quartz build...${NOCOLOR}\n"
+        npx quartz build -o /opt/public
+        printf "${MAGENTA}Quartz initial build complete${NOCOLOR}\n"
+
+else
+    printf "${MAGENTA}Quartz is already initialized. Checking for updates...${NOCOLOR}\n"
+    npx quartz update
+    printf "${MAGENTA}Performing quartz build...${NOCOLOR}\n"
+    npx quartz build -o /opt/public
+    printf "${MAGENTA}Quartz build complete${NOCOLOR}\n"
+fi
+
+printf "${CYAN}You can view your Quartz site at: ${NOCOLOR}http://localhost:80\n"
+
+while true; do
+        # inotifywait -r -e modify,create,delete,move /opt/quartz/content
+        # npx quartz build
+
+        sleep 10
+done
