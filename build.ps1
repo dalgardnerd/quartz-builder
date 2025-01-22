67b79ced8a33bc4c6449df16a8e5b3d5e321977e
dalgardnerd <thatiphoneguy@gmail.com>
2025-01-22 15:32:41 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,2 @@
-docker buildx build ./quartz-builder --% -t dalgardnerd:quartz-builder:latest
-docker buildx build ./quartz-caddy --% -t dalgardnerd:quartz-caddy:latestdocker buildx build ./quartz-builder --% -t dalgardnerd/quartz-builder:latest
 docker buildx build ./quartz-builder --% -t dalgardnerd/quartz-builder:latest
 docker buildx build ./quartz-caddy --% -t dalgardnerd/quartz-caddy:latest
